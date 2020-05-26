1ce20d794795baa834da28546ba50e22be9a10fe
bilhares <felipe.bilhares@hotmail.com>
2020-05-26 10:56:24 -0400

    initial

---
+++ LinqTeste/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fonte de dados
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            //consulta
+            System.Collections.Generic.List<int> list = numbers.Where(x => x % 2 == 0).ToList();
+
+            foreach(int x in list)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
